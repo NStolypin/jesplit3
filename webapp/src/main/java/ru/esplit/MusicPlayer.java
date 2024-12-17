ba307e7fb1b1598a9da2f0e44544ed56aa1fa809
NStolypin <i@nstolypin.ru>
2024-12-17 13:11:07 +0300

    properties

--- webapp/src/main/java/ru/esplit/MusicPlayer.java
+++ webapp/src/main/java/ru/esplit/MusicPlayer.java
@@ -3,6 +3,12 @@ package ru.esplit;
 public class MusicPlayer {
     private Music music;
 
+    private String name;
+    private int volume;
+
+    
+    public MusicPlayer() {}
+
     //IoC
     public MusicPlayer(Music music) {
         this.music = music;
@@ -10,5 +16,25 @@ public class MusicPlayer {
 
     public void playMusic() {
         System.out.println("Playing: " + music.getSong());
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return this.volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
     }
 }
